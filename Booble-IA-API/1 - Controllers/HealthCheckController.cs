f9a17d6cfcb209b2a0a5a765e7a9ee237d4409d7
joaocosta <bilin@DESKTOP-RHAOKAC>
2025-07-08 20:46:12 -0300

    ADD: HealthCheck

---
+++ Booble-IA-API/1 - Controllers/HealthCheckController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Booble_IA_API._1___Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class HealthCheckController : ControllerBase
+    {
+        [HttpGet]
+        [Route("HealthCheck")]
+        public IActionResult HealthCheck()
+        {
+            return Ok(new { Status = "Healthy", Timestamp = DateTime.UtcNow });
+        }
+    }
+}
